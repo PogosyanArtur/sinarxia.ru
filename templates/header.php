7fc221319dd5377c5c8d7733a4cbf84f10173e25
Artur Pogosyan <pa160290@outlook.com>
2019-07-22 09:37:08 +0300

    Refactoring footer and header

--- templates/header.php
+++ templates/header.php
@@ -5,27 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-
-        <!-- Yandex.Metrika counter -->
-
-        <script type="text/javascript" >
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-            ym(54391741, "init", {
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-            });
-        </script>
-        
-        <noscript>
-            <div><img src="https://mc.yandex.ru/watch/54391741" style="position:absolute; left:-9999px;" alt="" /></div>
-        </noscript>
-
-        <!-- /Yandex.Metrika counter -->
+        <meta name="yandex-verification" content="8235b6e2ebc17a4d" />
 
         <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
         <?php wp_head(); ?>
